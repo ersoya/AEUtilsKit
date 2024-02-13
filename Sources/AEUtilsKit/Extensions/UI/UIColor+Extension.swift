2b4f9b0af2e2066fa8f895a2ece410421fd8c0e3
Arda Ersoy <ardaaersoy@gmail.com>
2024-02-13 17:22:08 +0300

    Add ui color extension

---
+++ Sources/AEUtilsKit/Extensions/UI/UIColor+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIColor+Extension.swift
+//
+//
+//  Created by Arda Ersoy on 13.02.2024.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    convenience init(hex: String, alpha: CGFloat = .one) {
+        var hexValue = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexValue.hasPrefix("#") {
+            hexValue.remove(at: hexValue.startIndex)
+        }
+        
+        var rgbValue: UInt64 = .zero
+        Scanner(string: hexValue).scanHexInt64(&rgbValue)
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
