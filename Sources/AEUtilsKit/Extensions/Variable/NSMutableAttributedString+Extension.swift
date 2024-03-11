964c661722fb9ddca8297bd512e680a15e69a05b
Arda Ersoy <ardaaersoy@gmail.com>
2024-03-12 00:39:57 +0300

    Add nsmutableattributedstring extension

--- Sources/AEUtilsKit/Extensions/Variable/NSMutableAttributedString+Extension.swift
+++ Sources/AEUtilsKit/Extensions/Variable/NSMutableAttributedString+Extension.swift
@@ -10,6 +10,34 @@ import UIKit
 
 public extension NSMutableAttributedString {
     
+    func bold(_ value: String, font: UIFont? = nil) -> NSMutableAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font ?? .systemFont(ofSize: 14, weight: .semibold)
+        ]
+
+        append(NSAttributedString(string: value, attributes: attributes))
+        return self
+    }
+
+    func normal(_ value: String, font: UIFont? = nil) -> NSMutableAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font ?? .systemFont(ofSize: 14)
+        ]
+
+        append(NSAttributedString(string: value, attributes: attributes))
+        return self
+    }
+
+    func underlined(_ value: String, font: UIFont? = nil) -> NSMutableAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font ?? .systemFont(ofSize: 14),
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+
+        append(NSAttributedString(string: value, attributes: attributes))
+        return self
+    }
+    
     func paragraph(_ value: String) -> NSMutableAttributedString {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = .five
