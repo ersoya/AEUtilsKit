d98f6b79efdb8eb0677651af7e3d30d381e6445a
Arda Ersoy <ardaaersoy@gmail.com>
2024-03-12 00:37:00 +0300

    Add string extension for regular expression

--- Sources/AEUtilsKit/Extensions/Variable/String+Extension.swift
+++ Sources/AEUtilsKit/Extensions/Variable/String+Extension.swift
@@ -59,3 +59,12 @@ public extension String {
         return ceil(boundingBox.width)
     }
 }
+
+public extension String {
+
+    static func ~= (lhs: String, rhs: String) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
+        let range = NSRange(location: .zero, length: lhs.utf16.count)
+        return regex.firstMatch(in: lhs, options: [], range: range) != nil
+    }
+}
