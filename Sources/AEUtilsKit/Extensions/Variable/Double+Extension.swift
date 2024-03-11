d0e12c7df6d535079b96dc54c5f459ee6768f4f0
Arda Ersoy <ardaaersoy@gmail.com>
2024-03-12 00:42:39 +0300

    Add double extension

---
+++ Sources/AEUtilsKit/Extensions/Variable/Double+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Double+Extension.swift
+//
+//
+//  Created by Arda Ersoy on 12.03.2024.
+//
+
+import Foundation
+
+public extension Double {
+
+    var intValue: Int {
+        Int(self)
+    }
+}
