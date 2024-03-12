e9ae6f039567736f9fdb5004e8f3e6e1a8bda111
Arda Ersoy <ardaaersoy@gmail.com>
2024-03-12 03:22:32 +0300

    Update int constants

--- Sources/AEUtilsKit/Constants/Int+Constants.swift
+++ Sources/AEUtilsKit/Constants/Int+Constants.swift
@@ -10,4 +10,9 @@ import Foundation
 public extension Int {
     static let one: Int = 1
     static let two: Int = 2
+    static let three: Int = 3
+    static let four: Int = 4
+    static let six: Int = 6
+    static let ten: Int = 10
+    static let sixty: Int = 60
 }
