6aa38b32c7181d254bae9101aa824299e8a52ab8
Arda Ersoy <ardaaersoy@gmail.com>
2024-03-15 04:39:05 +0300

    Add scroll view extension

--- Sources/AEUtilsKit/Extensions/UI/UIScrollView+Extension.swift
+++ Sources/AEUtilsKit/Extensions/UI/UIScrollView+Extension.swift
@@ -17,3 +17,43 @@ public extension UIScrollView {
         scrollRectToVisible(frame, animated: true)
     }
 }
+
+public extension UIScrollView {
+
+    private enum AssociatedObjectKeys {
+        static var onRefreshAction = "onRefreshAction"
+    }
+
+    private typealias Action = () -> Void
+
+    private var onRefreshAction: Action? {
+        get {
+            let onRefreshActionInstance = objc_getAssociatedObject(self, &AssociatedObjectKeys.onRefreshAction) as? Action
+            return onRefreshActionInstance
+        }
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(self, &AssociatedObjectKeys.onRefreshAction, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            }
+        }
+    }
+
+    static var onRefresh: (() -> Void)?
+
+    func addRefreshControl(_ onRefresh: (() -> Void)?) {
+        onRefreshAction = onRefresh
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(didRefresh), for: .valueChanged)
+    }
+
+    @objc func didRefresh() {
+        if let action = onRefreshAction {
+            action()
+        }
+    }
+
+    func endRefreshing() {
+        refreshControl?.endRefreshing()
+        onRefreshAction = nil
+    }
+}
