ae35cb21db034820da3e709b0cf71459dae36b1b
Arda Ersoy <ardaaersoy@gmail.com>
2026-03-23 22:37:57 +0300

    Add decimal extension

---
+++ Sources/AEUtilsKit/Extensions/Variable/Decimal+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Decimal+Extension.swift
+//  AEUtilsKit
+//
+//  Created by Arda Ersoy on 23.03.2026.
+//
+
+import Foundation
+
+public extension Decimal {
+    
+    var toString: String {
+        "\(self)"
+    }
+}
